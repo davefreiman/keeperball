1a1b6ec2e2bec0f7d1319860e911137927938b03
David Freiman <dfreiman@demacmedia.com>
2016-04-09 10:36:32 -0400

    Remove activerecord bye

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,15 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+require "rails"
+require "active_model/railtie"
+# require "active_job/railtie"
+# require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "sprockets/railtie"
 
+# require "rails/test_unit/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -23,6 +31,5 @@ module Keeperball
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
   end
 end
